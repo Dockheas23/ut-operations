058d05fa25ad761675cde91dd64600076c34760a
George Boyle <george@thebuds.net>
2015-02-23 23:54:00 +0000

    Added pod file and updated to use YAML instead of JSON

--- ut-haskell/deploy.sh
+++ ut-haskell/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# NEED TO REMOVE POD
+# NEED TO REMOVE POD: Try --purge-config-cache
 gcloud preview container replicationcontrollers -n ut-cluster delete ut-haskell
 gcloud preview container replicationcontrollers -n ut-cluster create \
-    --config-file ut-haskell.replication.json
+    --config-file ut-haskell.replication.yaml

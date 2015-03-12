3148a3be9b67cb20a54d3dca8fe43e7a5245e5b9
George Boyle <george@thebuds.net>
2015-03-12 23:26:58 +0000

    Added blue/green deployment for ut-haskell

--- ut-haskell/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# NEED TO REMOVE POD: Try --purge-config-cache
-gcloud preview container replicationcontrollers -n ut-cluster delete ut-haskell
-gcloud preview container replicationcontrollers -n ut-cluster create \
-    --config-file ut-haskell.replication.yaml
--- ut-haskell/update.sh
+++ ut-haskell/update.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
-gcloud preview container kubectl update -f $(dirname $0)/ut-haskell.pod.yaml
+show_state() {
+    gcloud preview container kubectl get $1 | grep ut-haskell
+}
+
+wait_for_pods() {
+    show_state rc
+    echo "Waiting 10 seconds for pod to be removed"
+    sleep 10
+    echo "Current pod state..."
+    show_state pods
+}
+
+FROM=$(show_state rc | cut -d' ' -f1 | sed 's/ut-haskell-//')
+if [ $FROM == 'blue' ]; then
+    TO=green
+else
+    TO=blue
+fi
+
+echo "======================================="
+echo "Switching from $FROM to $TO deployment"
+echo "======================================="
+
+echo "Reducing replica size of $FROM to 0..."
+gcloud preview container kubectl update rc ut-haskell-$FROM \
+    --patch='{"apiVersion": "v1beta1", "desiredState": {"replicas": 0}}'
+
+wait_for_pods
+
+echo "Creating replication controller for ${TO}..."
+gcloud preview container kubectl create -f ut-haskell.repl.${TO}.yaml
+
+wait_for_pods
+
+echo "Removing $FROM replication controller"
+gcloud preview container kubectl delete rc ut-haskell-$FROM
